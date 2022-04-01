8d7ff78b772844b1f343f36190d3ded222372f86
THEXTREMESPECTR <93404684+THEXTREMESPECTR@users.noreply.github.com>
2022-04-01 16:39:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function goNow0(){
+        window.location='https://thextremespectr.github.io/RandomSupremePotato/index.html';
+    }
+function goNow1(){
+  window.location='https://not-a-rickroll.hyp3rkatana.repl.co/'
+}
+function goNow2(){
+ window.location="https://WIP.hyp3rkatana.repl.co"; 
+}
+function goNow3(){
+  alert("Github:THEXTREMESPECTR \nReplit: HYP3RKATANA")
+}
